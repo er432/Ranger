6aba9975d162e11f810c12969fbaf12ec0af3d2a
Eli Rodgers-Melnick <er432@cornell.edu>
2014-07-17 18:52:06 -0400

    Finishing versioning changes

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
+#!/usr/bin/env python
 from setuptools import setup, find_packages
 import os
+import sys
 
-here = os.path.abspath(os.path.dirname(__file__))
-
+sys.path.insert(0,'Ranger')
+import release
+version = release.write_versionfile()
+sys.path.pop(0)
+packages = ['Ranger',
+            'Ranger.src',
+            'Ranger.src.Range'
+           ]
 setup(
-    name='Ranger',
-    author='Eli Rodgers-Melnick',
-    author_email='er432@cornell.edu',
-    description='Generic library for dealing with ranges',
-    packages=['Ranger',
-              'Ranger.src',
-              'Ranger.src.Range'
-              ],
-    include_package_data=True,
-    platforms='any',
-    classifiers = [
-        'Programming Language :: Python',
-        'Natural Language :: English',
-        'Operating System :: OS Independent'
-        ]
+    name = release.name.lower(),
+    version = version,
+    author = release.authors['Rodgers-Melnick'][0],
+    author_email = release.authors['Rodgers-Melnick'][1],
+    description = release.description,
+    keywords = release.keywords,
+    long_description = release.long_description,
+    license = release.license,
+    platforms = release.platforms,
+    url = release.url,
+    classifiers = release.classifiers
     )
